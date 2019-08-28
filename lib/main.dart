a8174d4c11b5303baec37fa14c20b31e5b03c249
Stanley Wang <syw2007@gmail.com>
2019-08-27 21:57:39 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(title: 'Orient'),
+      routes: <String, WidgetBuilder>{
+        '/home':(context)=>MyHomePage(),
+        '/run':(context)=>RunPage(),
+        '/about':(context)=>AboutPage(),
+        '/test':(context)=>TestPage()
+
+      }
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('Welcome to Orient')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Orient'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Run Test'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.push(context, MaterialPageRoute(builder: (context) =>RunPage()));
+              },
+            ),
+            ListTile(
+              title: Text('About'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.push(context, MaterialPageRoute(builder: (context) =>AboutPage()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RunPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Run')),
+        body: RaisedButton(
+        child: const Text('Begin Test'),
+        color: Theme.of(context).accentColor,
+        elevation: 4.0,
+        splashColor: Colors.blueGrey,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) =>TestPage()));
+         },
+),
+        
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Orient'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Run Test'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>RunPage()));
+                },
+              ),
+              ListTile(
+                title: Text('About'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>AboutPage()));
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
+
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    {
+      return Scaffold(
+        appBar: AppBar(title: Text('About')),
+        body: Center(child: Text('Hemlo Who iz WE? well we is here to help you deal with your bad walking habits so watch out')),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Orient'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Run Test'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>RunPage()));
+                },
+              ),
+              ListTile(
+                title: Text('About'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
+}
+
+class TestPage extends StatelessWidget {
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Test')),
+    body:
+    Text('ouch my knee ouch my knee ouch my knee ouch my knee'));
+}
+}
+
+
